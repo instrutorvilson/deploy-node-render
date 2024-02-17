5d9d4eac240461c4ec0fc955f6a6d5e4652b8d14
Vilson Moro <vilson.moro@PROWAY.TREINA>
2024-02-17 09:10:07 -0300

    Criar endpoint de usuarios

--- scripts.sql
+++ scripts.sql
@@ -2,5 +2,13 @@ create table tb_categorias(
   id serial primary key,
   descricao varchar(20)
 )
-insert into tb_categorias(descricao)values('cereais')
-select * from tb_categorias
+insert into tb_categorias(descricao)values('cereais');
+select * from tb_categorias;
+
+create table tb_usuarios(
+  id serial primary key,
+  nome varchar(20),
+  email varchar(50),
+  senha varchar(200),
+  perfil varchar(10)
+);
--- usuarios.js
+++ usuarios.js
@@ -1,14 +1,21 @@
 const express = require('express')
 router = express.Router()
+const pg = require('pg')
 
-router.post('/register', (req, res) => {
-    let user = { 
-        nome: req.body.nome,
-        email: req.body.email,
-        senha: req.body.senha,
-        perfil: req.body.perfil
-    }
-    res.send({message:"registrar",body: user})
+const stringConexao = process.env.DATABASE_URL || 'postgres://postgres:admin@localhost/bd_node'
+const pool = new pg.Pool({ connectionString: stringConexao })
+
+router.post('/register', async(req, res) => {
+    try{
+        var client = await pool.connect() 
+        let sql = 'insert into tb_usuarios(nome, email, senha, perfil)values($1, $2, $3, $4) RETURNING *'
+        let dados = [req.body.nome, req.body.email, req.body.senha, req.body.perfil]
+        let user = await client.query(sql, dados)
+        res.status(201).send(user.rows[0])
+
+    }catch(error){
+        res.status(400).send(`Erro:${error.message}`)
+    }    
 })
 
 router.post('/login', (req, res) => {
