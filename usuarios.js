71f6d921a1b6263540c3ff8089f345e03f223079
Vilson Moro <vilson.moro@PROWAY.TREINA>
2024-02-17 09:59:22 -0300

    Verificar se usuário ja existe

--- usuarios.js
+++ usuarios.js
@@ -1,15 +1,24 @@
 const express = require('express')
 router = express.Router()
 const pg = require('pg')
+const bcrypt = require('bcrypt')
 
 const stringConexao = process.env.DATABASE_URL || 'postgres://postgres:admin@localhost/bd_node'
 const pool = new pg.Pool({ connectionString: stringConexao })
 
+
 router.post('/register', async(req, res) => {
     try{
-        var client = await pool.connect() 
+        let client = await pool.connect() 
+        let dados = await client.query('select * from tb_usuarios where email = $1',[req.body.email])
+
+        if(dados.rowCount > 0){
+            throw new Error('Email já cadastrado')
+        }
+
+        let hash = await bcrypt.hash(req.body.senha, 10)
         let sql = 'insert into tb_usuarios(nome, email, senha, perfil)values($1, $2, $3, $4) RETURNING *'
-        let dados = [req.body.nome, req.body.email, req.body.senha, req.body.perfil]
+        dados = [req.body.nome, req.body.email, hash, req.body.perfil]
         let user = await client.query(sql, dados)
         res.status(201).send(user.rows[0])
 
